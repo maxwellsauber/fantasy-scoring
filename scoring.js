2ed484da624371254dc939948dfbb5f2377dc388
Max Sauber <maxsauber@Maxs-Air.fios-router.home>
2020-03-14 17:06:51 -0400

    script skeleton

--- scoring.js
+++ scoring.js
@@ -1,4 +1,25 @@
 const calculateScore = (player) => {
-  return 40.72  // test first past
+  switch (player.position) {
+    case 'QB':
+      return quarterBackScore(player)
+    case 'RB':
+    case 'WR':
+      return runningBackOrWideReceiverScore(player)
+    default:
+      return 0
+  } 
+}
+
+const quarterBackScore = (player) =>{
+  const score = 0
+
+  return 40.72  // test first test
+}
+
+const runningBackOrWideReceiverScore = (player) => {
+  const score = 0
+  
+  return 32 // test second test
+
 }
 module.exports = calculateScore
