1da2e79a0d6d10533401619fc5bee7822f037cee
Max Sauber <maxsauber@Maxs-Air.fios-router.home>
2020-03-14 20:53:27 -0400

    RB WR Tests Pass

--- scoring.js
+++ scoring.js
@@ -5,6 +5,8 @@ const calculateScore = (player) => {
     case 'RB':
     case 'WR':
       return runningBackOrWideReceiverScore(player)
+    case "TE":
+      return tightEndScore()
     default:
       return 0
   }
@@ -26,6 +28,27 @@ const quarterBackScore = (player) => {
 const runningBackOrWideReceiverScore = (player) => {
   let score = 0
 
+  score += player.stats.rushing.yards / 10
+  score += player.stats.rushing.touchdowns * 6
+  score += player.stats.rushing.fumbles * -3
+  score += player.stats.receiving.receptions
+  score += player.stats.receiving.yards / 10
+  score += player.stats.receiving.touchdowns * 6
+  score += player.stats.receiving.fumbles * -3
+  score += player.stats.return.kickreturn.yards / 15
+  score += player.stats.return.kickreturn.touchdowns * 6
+  score += player.stats.return.kickreturn.fumbles * -3
+  score += player.stats.return.puntreturn.yards / 15
+  score += player.stats.return.puntreturn.touchdowns * 6
+  score += player.stats.return.puntreturn.fumbles * -3
+
   return score
 }
+
+const tightEndScore = (player) => {
+  let score = 0
+
+  return score
+}
+
 module.exports = calculateScore
